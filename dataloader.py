528540d6bb12a7d4a33f27de0d17edb8383c5c08
Hesham EL_Sherif <107759753+HeshamEL-Shreif@users.noreply.github.com>
2024-02-20 12:47:10 +0200

    feature_extraction, dataloader, master, object_detection, yolo8m.pt and feature_extraction.ipynb deleted

---
+++ dataloader.py
@@ -0,0 +1,152 @@
+
+import os
+import torch
+import cv2
+import torch.nn as nn
+from torch.utils.data import DataLoader, Dataset
+import torch.backends.cudnn as cudnn
+import numpy as np
+import torchvision
+import matplotlib.pyplot as plt
+from torchvision.transforms import ToTensor
+from torchvision import transforms
+from PIL import Image
+cudnn.benchmark = True
+plt.ion()
+
+
+class VideoFramesDataset(Dataset):
+    def __init__(self, data_path, desired_frames=100, desired_fps=10, transform=None):
+        self.desired_frames = desired_frames
+        self.desired_fps = desired_fps
+        self.data_path = data_path
+        self.classes = sorted(os.listdir(data_path))
+        self.class_to_idx = {cls_name: idx for idx, cls_name in enumerate(self.classes)}
+        self.idx_to_class = {idx: cls_name for cls_name, idx in self.class_to_idx.items()}
+        self.samples = self._make_dataset()
+        self.transform = transform or ToTensor()
+
+    def _make_dataset(self):
+        samples = []
+        for class_folder in self.classes:
+            class_path = os.path.join(self.data_path, class_folder)
+            if not os.path.isdir(class_path):
+                continue
+
+            class_idx = self.class_to_idx[class_folder]
+
+            for video_file in os.listdir(class_path):
+                video_path = os.path.join(class_path, video_file)
+                if not os.path.isfile(video_path):
+                    continue
+
+                samples.append((video_path, class_idx))
+
+        return samples
+
+    def _adjust_frames_and_fps(self, frames):
+        num_frames = frames.size(0)
+        current_fps = num_frames / self.duration
+
+        if current_fps < self.desired_fps:
+            increase_factor = int(self.desired_fps / current_fps)
+            frames = frames.repeat_interleave(increase_factor, dim=0)
+        elif current_fps > self.desired_fps:
+            reduction_factor = int(current_fps / self.desired_fps)
+            frames = frames[::reduction_factor]
+
+        if num_frames < self.desired_frames:
+            padding_frames = torch.zeros(self.desired_frames - num_frames, *frames.size()[1:], dtype=frames.dtype)
+            frames = torch.cat((frames, padding_frames))
+            del padding_frames
+        elif num_frames > self.desired_frames:
+            frames = frames[:self.desired_frames]
+
+        return frames
+
+    def __len__(self):
+        return len(self.samples)
+
+    def _read_frames(self, video_path):
+        cap = cv2.VideoCapture(video_path)
+        num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        duration = num_frames / cap.get(cv2.CAP_PROP_FPS)
+        del num_frames
+
+        frames = []
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
+
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            frame = Image.fromarray(frame)
+
+            if self.transform:
+                frame = self.transform(frame)
+            else:
+                frame = ToTensor()(frame)
+
+            frames.append(frame)
+            del frame
+
+        cap.release()
+        del cap
+
+        return torch.stack(frames), duration
+
+    def __getitem__(self, idx):
+        video_path, label = self.samples[idx]
+        frames, self.duration = self._read_frames(video_path)
+        frames = self._adjust_frames_and_fps(frames)
+
+        return frames, label
+
+
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+# function that load data pt file 
+def load_data(data_path, transform=transform, n_frames=100, fps=10, batch_size=1, shuffle=False):
+
+  video_dataset = VideoFramesDataset(data_path,
+                                     desired_frames=100,
+                                     desired_fps=10,
+                                     transform=transform)
+
+  data_loader = torch.utils.data.DataLoader(video_dataset, batch_size=batch_size, shuffle=shuffle)
+  del video_dataset
+  torch.save(data_loader, '/content/drive/MyDrive/dataset feature file/dataset_data_loader.pth')
+  return data_loader
+
+# load dataloader object from files
+def load_dataloader_from_files(path, batch_size = 1, transform=transform, shuffle = False):
+  loaded_dataset = torch.load(path)
+  reconstructed_dataloader = DataLoader(loaded_dataset,
+                                        batch_size=batch_size,
+                                        transform=transform,
+                                        shuffle=shuffle)
+  return reconstructed_dataloader
+
+# converts list to dataloader 
+class ListToDataSet(Dataset):
+    def __init__(self, data, labels):
+      self.data = data
+      self.labels = labels
+
+    def __len__(self):
+      return len(self.data)
+
+    def __getitem__(self, index):
+      data_point = self.data[index]
+      label = self.labels[index]
+      return data_point, label
+
+
+def list_to_dataloader(data, labels, batch_size, shuffle):
+  dataset = ListToDataSet(data, labels)
+  dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
+  return dataloader
---
+++ object_detection.py
@@ -0,0 +1,68 @@
+
+import os
+import torch
+import cv2
+import torch.nn as nn
+from torchvision import transforms
+import torch.backends.cudnn as cudnn
+import numpy as np
+import torchvision
+import matplotlib.pyplot as plt
+from PIL import Image
+from dataloader import *
+from feature_extraction import *
+cudnn.benchmark = True
+plt.ion()
+from ultralytics import YOLO
+
+yolov8_transform = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.Resize((640, 640)),
+    transforms.ToTensor(),
+])
+
+
+def object_level_feature_extraction(feature_extractor, object_detector, data_loader, batch_size = 32, shuffle = False, n_objects=5):
+    dataset_object_feature = []
+    dataset_labels = []
+    with torch.no_grad():
+
+      for batch in data_loader:
+          videos_frames, labels = batch
+          for video in videos_frames:
+            frames_objects_feat = []
+            for frame in video:
+              frame = yolov8_transform(frame)
+              results = object_detector(frame.unsqueeze(0), max_det=n_objects, save_crop=True)
+              cropped_regions = torch.zeros((n_objects, 1, feature_extractor.dim_feat))
+
+              for result in results:
+                boxes = result.boxes
+                for i, box in enumerate(boxes.xyxy):
+                  x1, y1, x2, y2 = box
+                  cropped_region = frame[ :, int(y1.item()): int(y2.item()), int(x1.item()):int(x2.item())]
+                  cropped_region = vgg16_transform(cropped_region)
+                  cropped_region = cropped_region.unsqueeze(0)
+                  cropped_region = feature_extractor(cropped_region.to(torch.device('cuda')))
+                  cropped_regions[i] = cropped_region
+    
+                frames_objects_feat.append(cropped_regions)
+                del(cropped_regions)
+                torch.cuda.empty_cache() 
+                
+            frames_objects_feat = torch.stack(frames_objects_feat)
+
+            dataset_object_feature.append(frames_objects_feat)
+            del(frames_objects_feat)
+            torch.cuda.empty_cache() 
+          dataset_labels.extend(labels.tolist())
+      object_feature_dataloader = list_to_dataloader(dataset_object_feature,
+                                                    dataset_labels,
+                                                    batch_size,
+                                                    shuffle)
+      torch.save(object_feature_dataloader, '/content/drive/MyDrive/dataset feature file/object_feature_dataloader.pth')
+    return object_feature_dataloader
+
+
+
+
