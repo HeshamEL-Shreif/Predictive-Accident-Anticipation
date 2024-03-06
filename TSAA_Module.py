0213f6b4f4d203b6478f21c6bdb94fd2aa6dfa8f
Hesham EL_Sherif <107759753+HeshamEL-Shreif@users.noreply.github.com>
2024-03-06 14:55:49 +0200

    tsaa module file added

---
+++ TSAA_Module.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+
+
+class SelfAttentionAggregation(nn.Module):
+    def __init__(self, device, t, d):
+        super().__init__()
+        self.w_saa = torch.randn((1, t), device=device, requires_grad=True) * 0.01
+        self.dense1 = nn.Linear(d, 64)
+        self.dense2 = nn.Linear(64, 2)
+
+    
+    def forward(self, h_v):
+        z_v = nn.Softmax()(torch.matmul(h_v.T , h_v))
+        z_v = torch.matmul(h_v, z_v)
+        z_v = torch.matmul(self.w_saa, z_v)
+        a_v = self.dense1(z_v.squeeze(0))
+        a_v = self.dense2(a_v)
+        a_v = nn.Softmax()(a_v)
+        return a_v
